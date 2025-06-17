54911c4520a8414201500347fd0c94e5e3b3cd0f
dev-kt07 <krishnatonk939@gmail.com>
2025-06-17 17:04:13 +0530

    new config

--- main.tf
+++ main.tf
@@ -8,8 +8,12 @@ provider "aws" {
 module "eks" {
   source         = "./modules/eks"
   cluster_name   = "dev-eks-cluster"
-  cluster_version = "1.29"
+  kubernetes_version = "1.29"
   vpc_id         = "vpc-0219578dc5af3223e"
   subnet_ids     = [ "subnet-06053f1ddc9a129a2", "subnet-043fd9105126cf4a5" ]
   ssh_key_name   = "github-action"
+   desired_size   = 2
+  min_size       = 1
+  max_size       = 2
+  instance_types = ["t2.medium"]
 }
