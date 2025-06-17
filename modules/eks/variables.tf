e160ab67622c9915699384ad459122022022db0f
dev-kt07 <krishnatonk939@gmail.com>
2025-06-17 17:10:43 +0530

    add this

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,24 +1,42 @@
 variable "cluster_name" {
-    type = string
-    description = "this is a demo eks cluster"
-    default = "dev-eks-cluster"
+  type        = string
+  description = "this is a demo eks cluster"
+  default     = "dev-eks-cluster"
 }
-variable "cluster_version" {
-  default = "1.29"
-}
+
 variable "subnet_ids" {
-  type = list(string)
+  type        = list(string)
   description = "subnet ids for eks"
-  default = [ "subnet-06053f1ddc9a129a2", "subnet-043fd9105126cf4a5" ]
+  default     = ["subnet-06053f1ddc9a129a2", "subnet-043fd9105126cf4a5"]
 }
 variable "vpc_id" {
-     type = string
-    description = "this is a demo eks cluster"
-    default = "vpc-0219578dc5af3223e"
+  type        = string
+  description = "this is a demo eks cluster"
+  default     = "vpc-0219578dc5af3223e"
 }
 variable "ssh_key_name" {
-      type = string
-    description = "this is a demo eks cluster"
-    default = "github-action"
-    
+  type        = string
+  description = "this is a demo eks cluster"
+  default     = "github-action"
+
+}
+variable "desired_size" {
+  type    = number
+  default = 2
+}
+variable "max_size" {
+  type    = number
+  default = 2
+}
+variable "min_size" {
+  type    = number
+  default = 1
+}
+variable "instance_types" {
+  type    = list(string)
+  default = ["t2.medium"]
+}
+variable "kubernetes_version" {
+  type    = string
+  default = "1.29"
 }
